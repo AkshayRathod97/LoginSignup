aca7c04755173c4bace62a6a2cc5f8e33fe19abc
Akshay Rathod <aksratho@amdocs.com>
2021-11-11 09:04:28 +0530

    comment for code changes

---
+++ script.js
@@ -0,0 +1,37 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
+});
+
+function loginPress(){
+    var data = document.getElementById("loginForms")
+    loginId = data.elements['loginId'].value;
+    password = data.elements['password'].value;
+    if(localStorage.getItem(loginId) == password){
+        alert("credentials valid !!")
+    }
+    else {
+        alert("invalid credentials, Sign Up first !!!")
+    }    
+}
+
+function signUpPress(){
+    var data = document.getElementById("signupForms")
+    loginId = data.elements['inputLoginId'].value;
+    password = data.elements['inputPassword'].value;
+    localStorage.setItem(loginId,password)
+    alert("Onboarded !!!")
+}
